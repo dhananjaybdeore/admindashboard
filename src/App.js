3d3d8624d8799e4c9155257619fdd9c0d1caa185
dhananjaybdeore <95953902+dhananjaybdeore@users.noreply.github.com>
2023-02-22 19:15:18 +0530

    Users page completed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
 import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
+    <Router className="App">
       <Topbar />
+      {/* <UserList /> */}
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          {/* <Route  path="/users" element={<UserList />}> */}
+          <Route exact path="/users" element={<UserList />}></Route>
+          <Route exact path="/user/:userId" element={<User />}></Route>
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
